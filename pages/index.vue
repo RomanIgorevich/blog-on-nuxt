9ff0ae8869d588163a5ccfed4e607c4896a6f057
Roman Igorevich <roman.igorevich.ru@gmail.com>
2021-11-01 11:14:05 +0300

    домашняя страничка

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <div class="wrapper-content wrapper-content--fixed"></div>
+  <div class="wrapper-content wrapper-content--fixed">
+    <promo />
+    <intro title="опять я вызвал интро" />
+  </div>
 </template>
+
+<script>
+import Promo from "../components/Promo.vue";
+import Intro from "../components/UI/Intro.vue";
+export default {
+  components: {
+    Promo,
+    Intro,
+  },
+};
+</script>
