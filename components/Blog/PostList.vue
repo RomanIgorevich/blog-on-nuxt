0cea51623bce1cb457d2702dbd0cd6e0ef994a71
Roman Igorevich <roman.igorevich.ru@gmail.com>
2021-11-01 12:35:40 +0300

    вывод постов

---
+++ components/Blog/PostList.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="post-list">
+    <div class="container">
+      <div class="posts__wrapper">
+        <post-preview v-for="post in posts" :key="post.id" :post="post">
+        </post-preview>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import PostPreview from "./PostPreview.vue";
+export default {
+  components: {
+    PostPreview,
+  },
+  props: {
+    posts: {
+      type: Array,
+      requried: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.posts__wrapper {
+  display: flex;
+  align-items: start;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,44 @@
 <template>
   <div class="wrapper-content wrapper-content--fixed">
-    <promo />
+    <Promo />
     <intro title="опять я вызвал интро" />
+    <post-list :posts="posts" />
   </div>
 </template>
 
 <script>
 import Promo from "../components/Promo.vue";
 import Intro from "../components/UI/Intro.vue";
+import PostList from "../components/Blog/PostList.vue";
 export default {
   components: {
     Promo,
     Intro,
+    PostList,
+  },
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: "1 пост",
+          descr: "Описание первого поста",
+          img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRpgflEenkGo0V61m04Ky-njYSMcg0LYAzfAcmfHWCBeplyuJ6PRhQSp1rpwEVEyNgatA&usqp=CAU",
+        },
+        {
+          id: 2,
+          title: "2 пост",
+          descr: "Описание первого поста",
+          img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRpgflEenkGo0V61m04Ky-njYSMcg0LYAzfAcmfHWCBeplyuJ6PRhQSp1rpwEVEyNgatA&usqp=CAU",
+        },
+        {
+          id: 3,
+          title: "3 пост",
+          descr: "Описание первого поста",
+          img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRpgflEenkGo0V61m04Ky-njYSMcg0LYAzfAcmfHWCBeplyuJ6PRhQSp1rpwEVEyNgatA&usqp=CAU",
+        },
+      ],
+    };
   },
 };
 </script>
