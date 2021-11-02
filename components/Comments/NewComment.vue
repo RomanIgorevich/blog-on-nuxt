a2102758cdd84c07068401a333d3c95f066555e0
Roman Igorevich <roman.igorevich.ru@gmail.com>
2021-11-02 11:25:34 +0300

    message

--- components/Comments/NewComment.vue
+++ components/Comments/NewComment.vue
@@ -2,6 +2,7 @@
   <section class="new-comment">
     <div class="container">
       <h2 class="title">Новыи коментарии</h2>
+      <Message v-if="message" :message="message" />
       <form @submit.prevent="onSubmit" class="contact-form">
         <AppInput v-model="comment.name">Имя:</AppInput>
         <AppTextAria v-model="comment.text">Text:</AppTextAria>
@@ -17,9 +18,12 @@
 import AppButton from "@/components/UI/Controls/Button.vue";
 import AppInput from "@/components/UI/Controls/Input.vue";
 import AppTextAria from "@/components/UI/Controls/TextAria.vue";
+import Message from "@/components/UI/Message.vue";
+
 export default {
   data() {
     return {
+      message: null,
       comment: {
         name: "",
         text: "",
@@ -30,10 +34,13 @@ export default {
     AppButton,
     AppInput,
     AppTextAria,
+    Message,
   },
   methods: {
     onSubmit() {
-      console.log(this.comment);
+      this.message = "Отправлено!";
+      this.comment.name = "";
+      this.comment.text = "";
     },
   },
 };
--- components/Contacts.vue
+++ components/Contacts.vue
@@ -2,6 +2,7 @@
   <section class="contact">
     <div class="container">
       <h2 class="title">Отправить сообщение</h2>
+      <Message v-if="message" :message="message" />
       <form @submit.prevent="onSubmit" class="contact-form">
         <AppInput v-model="user.name">Имя:</AppInput>
         <AppInput v-model="user.email" type="email">Email:</AppInput>
@@ -18,10 +19,12 @@
 import AppButton from "@/components/UI/Controls/Button.vue";
 import AppInput from "@/components/UI/Controls/Input.vue";
 import AppTextAria from "@/components/UI/Controls/TextAria.vue";
+import Message from "@/components/UI/Message.vue";
 
 export default {
   data() {
     return {
+      message: null,
       user: {
         name: "",
         email: "",
@@ -33,9 +36,11 @@ export default {
     AppButton,
     AppInput,
     AppTextAria,
+    Message,
   },
   methods: {
     onSubmit() {
+      this.message = "Отправлено!";
       this.user.name = "";
       this.user.email = "";
       this.user.text = "";
