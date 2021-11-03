6d5761836ede70dd10e3295ba3b7b72d0ddfc78d
Roman Igorevich <roman.igorevich.ru@gmail.com>
2021-11-03 12:11:21 +0300

    админ панель

---
+++ layouts/admin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <Header />
+    <div class="wrapper-content wrapper-content--fixed">
+      <Intro title="Cтраница администратора">
+        <nuxt-link to="/admin" class="link linkWhite"> Администратор</nuxt-link>
+        <nuxt-link to="/admin/new-post" class="link linkWhite">
+          Новый пост</nuxt-link
+        >
+        <nuxt-link to="/admin/comments" class="link linkWhite">
+          Коментарии
+        </nuxt-link>
+        <span class="link linkWhite"> Выход </span>
+      </Intro>
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/system/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ pages/admin/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <PostsList :posts="posts" />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "admin",
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: "1 пост",
+          descr: "Описание первого поста",
+          img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRpgflEenkGo0V61m04Ky-njYSMcg0LYAzfAcmfHWCBeplyuJ6PRhQSp1rpwEVEyNgatA&usqp=CAU",
+        },
+        {
+          id: 2,
+          title: "2 пост",
+          descr: "Описание первого поста",
+          img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRpgflEenkGo0V61m04Ky-njYSMcg0LYAzfAcmfHWCBeplyuJ6PRhQSp1rpwEVEyNgatA&usqp=CAU",
+        },
+        {
+          id: 3,
+          title: "3 пост",
+          descr: "Описание первого поста",
+          img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRpgflEenkGo0V61m04Ky-njYSMcg0LYAzfAcmfHWCBeplyuJ6PRhQSp1rpwEVEyNgatA&usqp=CAU",
+        },
+      ],
+    };
+  },
+};
+</script>
