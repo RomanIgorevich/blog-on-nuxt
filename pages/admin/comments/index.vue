bf6327bbb791deca8ac2459e1939487bfa096889
Roman Igorevich <roman.igorevich.ru@gmail.com>
2021-11-07 18:54:20 +0300

    коментарии

---
+++ pages/admin/comments/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <commentTable
+    :thead="['Имя', 'Текст', 'Статус', 'Изменить статус', 'Удалить']"
+  >
+    <tbody slot="tbody">
+      <tr v-for="comment in comments" :key="comment.id">
+        <td>
+          <span>{{ comment.name }}</span>
+        </td>
+        <td>
+          <span>{{ comment.text }}</span>
+        </td>
+        <td>
+          <span v-if="comment.publish" class="status true">Опубликованный</span>
+          <span v-else class="status false">Не опубликованный</span>
+        </td>
+        <td>
+          <span @click="changeComment(comment.id)" class="link">
+            Повесить статус</span
+          >
+        </td>
+        <td>
+          <span @click="deleteComment(comment.id)" class="link"> Удалить </span>
+        </td>
+      </tr>
+    </tbody>
+  </commentTable>
+</template>
+
+<script>
+import commentTable from "@/components/Admin/CommentTable.vue";
+export default {
+  components: {
+    commentTable,
+  },
+  layout: "admin",
+  data() {
+    return {
+      comments: [
+        { id: 1, name: "Роман", text: "asdasd asd asd as d sa" },
+        { id: 2, name: "Роман", text: "asdasd asd asd as d sa" },
+      ],
+    };
+  },
+
+  methods: {
+    changeComment(id) {
+      console.log(`изменить коментарии - ${id}`);
+    },
+    deleteComment(id) {
+      console.log(`удалить коментарии - ${id}`);
+    },
+  },
+};
+</script>
